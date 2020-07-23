18f6a89849aec552c8b4e2946af5b5a629487dcc
Eduard Bondarenko <eduardbcom@gmail.com>
2020-07-24 00:12:12 +0300

    Add parallel.DoWithLimit. Make parallel.Do truly parallel

--- README.md
+++ README.md
@@ -24,6 +24,26 @@ if err != nil {
 res == []interface{}{"task1", "task2"}
 ```
 
+```go
+import (
+    "github.com/eduardbcom/gasync/parallel"
+)
+
+ctx := context.TODO()
+
+res, err := parallel.DoWithLimit(
+    2,
+    // both functions run in parallel
+    func() (interface{}, error) { return request(ctx, "task1") },
+    func() (interface{}, error) { return request(ctx, "task2") },
+)
+if err != nil {
+    return nil, err
+}
+
+res == []interface{}{"task1", "task2"}
+```
+
 ## Motivation:
 At some point of my journey with Go I realized that I want to hide all concurrency mess under pleasurable interface.
 As long as I worked with JS for a while, the first idea was "I need something like the 'async' module. But in Go.".
--- parallel/parallel.go
+++ parallel/parallel.go
@@ -1,18 +1,68 @@
 package parallel
 
+import (
+	"sync"
+)
+
 // Parallel runs functions in parallel.
+//
 // If one of them returns an error, the final result is a (nil, Error).
+// The amount of function calls in that case is undefined.
+//
 // Otherwise, it's an array of results
 func Do(funcs ...func() (interface{}, error)) ([]interface{}, error) {
+	return DoWithLimit(len(funcs), funcs...)
+}
+
+// DoWithLimit runs maximum `limit` amount of functions in parallel.
+// 
+// If one of them returns an error, the final result is a (nil, Error).
+// The amount of function calls in that case is undefined.
+// 
+// Otherwise, it's an array of results.
+func DoWithLimit(limit int, funcs ...func() (interface{}, error)) ([]interface{}, error) {
+	if limit <= 0 {
+		panic("Incorrect limit value")
+	}
+
 	results := make([]interface{}, len(funcs))
 
+	var wg sync.WaitGroup
+
+	ch := make(chan bool, limit)
+	errorCh := make(chan error, limit)
+
 	for i, f := range funcs {
-		res, err := f()
-		if err != nil {
-			return nil, err
+		// limit amount of parallel functions
+		ch <- true
+
+		if len(errorCh) != 0 {
+			break
 		}
 
-		results[i] = res
+		wg.Add(1)
+
+		go func(i int, f func() (interface{}, error)) {
+			defer wg.Done()
+
+			res, err := f()
+			if err != nil {
+				errorCh <- err
+
+				// dont do <- in case of errors
+				// no reason to take more tasks, already error case
+				return
+			}
+
+			results[i] = res
+			<-ch
+		}(i, f)
+	}
+
+	wg.Wait()
+
+	if len(errorCh) != 0{
+		return nil, <-errorCh
 	}
 
 	return results, nil
